da97a82f9270e9855cc32c5b36918d3246034ca8
yonathan.huang <yonathan.huang@gmail.com>
2019-11-06 16:59:56 +0700

    PDF Shown d

--- src/main/java/com/kinnara/kecakplugins/digitalsignature/DigitalSignature.java
+++ src/main/java/com/kinnara/kecakplugins/digitalsignature/DigitalSignature.java
@@ -1,17 +1,63 @@
 package com.kinnara.kecakplugins.digitalsignature;
 
+import org.joget.apps.app.model.AppDefinition;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.model.Element;
 import org.joget.apps.form.model.FormBuilderPaletteElement;
 import org.joget.apps.form.model.FormData;
+import org.joget.apps.form.service.FileUtil;
 import org.joget.apps.form.service.FormUtil;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 public class DigitalSignature extends Element implements FormBuilderPaletteElement {
     @Override
     public String renderTemplate(FormData formData, Map dataModel) {
         String template = "digitalSignature.ftl";
+
+        String formDefId = (String) getProperty("formDefId");
+        String signatureId = (String) getProperty("fileField");
+        String username = (String) getProperty("username");
+
+        String appId = "";
+        String appVersion = "";
+
+        AppDefinition appDef = AppUtil.getCurrentAppDefinition();
+        if (appDef != null) {
+            appId = appDef.getId();
+            appVersion = appDef.getVersion().toString();
+        }
+
+        String primaryKeyValue = getPrimaryKeyValue(formData);
+
+        String value = FormUtil.getElementPropertyValue(this, formData);
+        if(value!=null) {
+            String encodedFileName = value;
+            try {
+                encodedFileName = URLEncoder.encode(value, "UTF8").replaceAll("\\+", "%20");
+            } catch (UnsupportedEncodingException ex) {
+                // ignore
+            }
+            if(encodedFileName!=null && !encodedFileName.equals("")) {
+                String fileLocation = FileUtil.getUploadPath(this, primaryKeyValue) + "/" + encodedFileName;
+                File file = new File(fileLocation);
+                if(file.exists()) {
+                    String filePath = "/web/client/app/" + appId + "/" + appVersion + "/form/download/" + formDefId + "/" + primaryKeyValue + "/" + encodedFileName + ".";
+                    dataModel.put("pdfFile", filePath);
+                }else {
+                    String filePath = "/web/client/app/" + appId + "/" + appVersion + "/form/download/" + formDefId + "/" + primaryKeyValue + "/" + value + ".";
+                    dataModel.put("pdfFile", filePath);
+                }
+            }else {
+                String filePath = "/web/client/app/" + appId + "/" + appVersion + "/form/download/" + formDefId + "/" + primaryKeyValue + "/" + value + ".";
+                dataModel.put("pdfFile", filePath);
+            }
+        }else{
+            dataModel.put("pdfFile", "");
+        }
         dataModel.put("className", getClassName());
         String html = FormUtil.generateElementHtml(this, formData, template, dataModel);
         return html;
@@ -34,7 +80,7 @@ public class DigitalSignature extends Element implements FormBuilderPaletteEleme
 
     @Override
     public String getFormBuilderTemplate() {
-        return "<label class='label' style='position:absolute;top:10px;left:10px;'>Digital Signature</label><div style='border: 5px solid grey;height:200px;background-color:#EFF1F2;color:#C4C7CB;align:center;'><span style='position:absolute;top:70px;left:270px;font-weight:bold;font-size:70px;align:center;'>PDF</span><div>";
+        return "<label class='label' style='position:absolute;top:10px;left:10px;'>Digital Signature</label><div style='border: 5px solid grey;height:100px;background-color:#EFF1F2;color:#C4C7CB;align:center;'><span style='position:absolute;top:10px;left:270px;font-weight:bold;font-size:70px;align:center;'>PDF</span><div>";
     }
 
     @Override
--- src/main/resources/templates/digitalSignature.ftl
+++ src/main/resources/templates/digitalSignature.ftl
@@ -1,12 +1,50 @@
 <#if includeMetaData!>
+	<label class='label' >Digital Signature</label>
 	<div class="form-cell" ${elementMetaData!}>
-	    <label class="label">${element.properties.label} <span class="form-cell-validator">${decoration}</span><#if error??> <span class="form-error-message">${error}</span></#if></label>
-	    <#if (element.properties.readonly! == 'true' && element.properties.readonlyLabel! == 'true') >
-	        <div class="form-cell-value"><span>${value!?html}</span></div>
-	        <input id="${elementParamName!}" name="${elementParamName!}" type="hidden" value="${value!?html}" />
-	    <#else>
-	        <input id="${elementParamName!}" name="${elementParamName!}" type="text" size="${element.properties.size!}" value="${value!?html}" maxlength="${element.properties.maxlength!}" <#if error??>class="form-error-cell"</#if> <#if element.properties.readonly! == 'true'>readonly</#if> />
-	    </#if>
+	    <label class="label" style="position:absolute;top:10px;left:10px;">
+			${element.properties.label} 
+			<span class="form-cell-validator">${decoration}</span>
+			<#if error??> <span class="form-error-message">${error}</span></#if>
+		</label>
+		<div style='border: 5px solid grey;height:100px;background-color:#EFF1F2;color:#C4C7CB;align:center;'>
+			<span style='position:absolute;top:10px;left:270px;font-weight:bold;font-size:70px;align:center;'>PDF</span>
+		<div>
 	</div>
 <#else>
+	<script type="text/javascript" src="${request.contextPath}/plugin/${className}/pdf.js"/></script>
+	<canvas id="pdf-canvas" width="100%" height="500px"></canvas>
+	
+	<script>
+		$(document).ready(function(){
+			var url = "${request.contextPath}${pdfFile!?html}";
+			
+			pdfjsLib.GlobalWorkerOptions.workerSrc = '${request.contextPath}/plugin/${className}/pdf.worker.js';
+			var loadingTask = pdfjsLib.getDocument(url);
+		  	loadingTask.promise.then(function(pdf) {
+				//
+				// Fetch the first page
+				//
+				pdf.getPage(1).then(function(page) {
+					var scale = 1.5;
+					var viewport = page.getViewport({ scale: scale, });
+					//
+					// Prepare canvas using PDF page dimensions
+					//
+					var canvas = document.getElementById('pdf-canvas');
+					var context = canvas.getContext('2d');
+					canvas.height = viewport.height;
+					canvas.width = viewport.width;
+					//
+					// Render PDF page into canvas context
+					//
+					var renderContext = {
+					canvasContext: context,
+					viewport: viewport,
+					};
+					page.render(renderContext);
+				});
+			});
+		});
+	
+	</script>
 </#if>
