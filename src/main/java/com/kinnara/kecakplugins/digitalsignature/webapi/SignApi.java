af39a8544fbd6a8ed6a94a9cd6d9de3e9a56cd34
Fina Luthfiyani <fina.luthfiyani@kinnarastudio.com>
2023-04-04 17:07:16 +0700

    wip : sign api

--- src/main/java/com/kinnara/kecakplugins/digitalsignature/webapi/SignApi.java
+++ src/main/java/com/kinnara/kecakplugins/digitalsignature/webapi/SignApi.java
@@ -2,26 +2,46 @@ package com.kinnara.kecakplugins.digitalsignature.webapi;
 
 import com.itextpdf.signatures.DigestAlgorithms;
 import com.itextpdf.signatures.PdfSigner;
+import com.kinnara.kecakplugins.digitalsignature.exception.DigitalCertificateException;
+import com.kinnara.kecakplugins.digitalsignature.exception.RestApiException;
 import com.kinnara.kecakplugins.digitalsignature.util.PKCS12Utils;
+import com.kinnarastudio.commons.Try;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.form.model.Form;
+import org.joget.apps.form.model.FormData;
+import org.joget.commons.util.FileManager;
+import org.joget.commons.util.FileStore;
 import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.ExtDefaultPlugin;
 import org.joget.plugin.base.PluginManager;
 import org.joget.plugin.base.PluginWebSupport;
 import org.joget.workflow.util.WorkflowUtil;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.kecak.apps.exception.ApiException;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.security.KeyPair;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.text.ParseException;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class SignApi extends ExtDefaultPlugin implements PluginWebSupport, PKCS12Utils {
     @Override
@@ -55,14 +75,23 @@ public class SignApi extends ExtDefaultPlugin implements PluginWebSupport, PKCS1
 
             // TODO sign pdf document
 
-            String userFullname = "user@gmail.com";
-            File pdfFile = new File("input_file.pdf");
+            LogUtil.info(getClass().getName(), "Executing Rest API [" + servletRequest.getRequestURI() + "] in method [" + servletRequest.getMethod() + "] contentType [" + servletRequest.getContentType() + "] as [" + WorkflowUtil.getCurrentUsername() + "]");
+
+            String fileToSign = servletRequest.getParameter("file");
+            String userFullname = servletRequest.getHeader("email");
+
+            LogUtil.info(getClass().getName(), "file :" + fileToSign);
+            LogUtil.info(getClass().getName(), "email :" + userFullname);
+
+            File pdfFile = new File(fileToSign);
             File userKeystoreFile = new File("input_file.pdf");
 
+            //SIGN PDF
+            generateUserKey(userKeystoreFile, getPassword(), userFullname);
             startSign(userKeystoreFile, pdfFile, userFullname, "Sign", "Kecak.org");
 
-
             final JSONObject responseBody = new JSONObject();
+            responseBody.put("signedFile", pdfFile);
             responseBody.put("status", false);
             responseBody.put("message", "UNDER DEVELOPMENT");
 
@@ -73,6 +102,24 @@ public class SignApi extends ExtDefaultPlugin implements PluginWebSupport, PKCS1
         } catch (JSONException e) {
             LogUtil.error(getClass().getName(), e, e.getMessage());
             servletResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (UnrecoverableKeyException | CertificateException | NoSuchAlgorithmException | KeyStoreException |
+                 ParseException | OperatorCreationException | DigitalCertificateException e) {
+            LogUtil.error(getClass().getName(), e, e.getMessage());
         }
+    }
+
+    public void generateUserKey(File certificateFile, char[] pass, String userFullname) throws NoSuchAlgorithmException, CertificateException, KeyStoreException, IOException, OperatorCreationException, ParseException, UnrecoverableKeyException, DigitalCertificateException {
+        KeyPair generatedKeyPair = generateKeyPair();
+        String subjectDn = getDn(userFullname, "", "Kecak", "ID", "West Java", "Bandung");
+        generatePKCS12(certificateFile, pass, generatedKeyPair, subjectDn, false);
+    }
+
+    protected String[] getTempFilePath(String elementId) {
+        return Optional.of(elementId)
+                .map(Try.onFunction(FileStore::getFiles))
+                .map(Arrays::stream)
+                .orElseGet(Stream::empty)
+                .map(FileManager::storeFile)
+                .toArray(String[]::new);
     }
 }
